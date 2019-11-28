72a0803f958bab971f65e5f467e878de55de5e73
AB2462 <56684094+AB2462@users.noreply.github.com>
2019-11-28 13:49:55 +0000

    Add files via upload

---
+++ desktop/src/com/kroy/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kroy.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kroy.game.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
